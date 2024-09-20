c67c8b1753e8c2d0a3ab2e1a83388895811b7fdb
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 23:01:03 +0200

    add button to temp search component

--- src/components/search-city/Search.jsx
+++ src/components/search-city/Search.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-export const Search = ({ search, setSearch }) => {
+export const Search = ({ search, setSearch, handleSearch }) => {
   return (
     <div className='search'>
       <input
         type='text'
         name='search'
-        className='city-search'
+        className='search-city'
         placeholder='Enter City name ...'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
+      <button className='search-button' onClick={handleSearch}>
+        Search Weather
+      </button>
     </div>
   );
 };
