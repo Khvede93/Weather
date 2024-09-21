5e03bc7aa75e30c3b8185512aaadfbdfe46153ed
Khvede93 <gio.khvede93@gmail.com>
2024-09-21 17:41:21 +0200

    working version of app is Done. (reading any city weather, showing only city and country on page)

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -45,11 +45,12 @@ export const Weather = () => {
       ) : (
         <div>
           <div className='city-name'>
-            <h2>{weatherData?.name}</h2>
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
           </div>
         </div>
       )}
-      <div>Weather</div>
     </div>
   );
 };
