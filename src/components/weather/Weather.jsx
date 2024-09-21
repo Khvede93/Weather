8a159c0b415826b7e7bb0a6d0e96257f5bde00ff
Khvede93 <gio.khvede93@gmail.com>
2024-09-21 03:37:02 +0200

    add states, fetching weather data from api

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Search } from '../search-city';
 
 export const Weather = () => {
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchWeatherData(param) {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=d12cb77f7b328824c97d05f086f85298`
+      );
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {}
+  }
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
   return (
     <div>
-      <Search />
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
       <div>Weather</div>
     </div>
   );
